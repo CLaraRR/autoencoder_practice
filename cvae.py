3a3d845bed5b159ec1e1205699f5e9ba8247fd30
CLaraRR <176367137@qq.com>
2020-04-03 19:28:48 +0800

    add cvae

---
+++ cvae.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+from keras.layers import Input, Dense, Lambda
+from keras.models import Model, load_model
+from keras import backend as K
+from keras.datasets import mnist
+from keras.utils import to_categorical
+import os
+
+# 指定gpu
+# os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+
+##### 设置网络参数 #####
+batch_size = 2048
+original_dim = 784
+latent_dim = 2
+intermediate_dim = 256
+epochs = 100
+num_classes = 10
+
+##### 加载mnist数据集
+(x_train, y_train_), (x_test, y_test_) = mnist.load_data('mnist.npz')
+x_train = x_train.astype('float32')/255.
+x_test = x_test.astype('float32')/255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+y_train = to_categorical(y_train_, num_classes)
+y_test = to_categorical(y_test_, num_classes)
+
+x = Input(shape=(original_dim,))
+h = Dense(intermediate_dim, activation='relu')(x)
+
+# 算p(Z|X)的均值 方差
+z_mean = Dense(latent_dim)(h)
+z_log_var = Dense(latent_dim)(h)
+
+y = Input(shape=(num_classes,))  # 输入类别
+yh = Dense(latent_dim)(y)  # 直接构建每个类别的均值
+
+# 重参数技巧
+def sampling(args):
+    z_mean, z_log_var = args
+    epsilon = K.random_normal(shape=K.shape(z_mean))
+    return z_mean + epsilon * K.exp(z_log_var / 2)
+
+# 重参数层，相当于给输入加入噪声
+z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
+
+# 解码层
+decoder_h = Dense(intermediate_dim, activation='relu')
+decoder_mean = Dense(original_dim, activation='sigmoid')
+h_decoded = decoder_h(z)
+x_decoded_mean = decoder_mean(h_decoded)
+
+# 建立模型
+vae = Model([x, y], [x_decoded_mean, yh])
+
+# xent是重构loss, kl_loss是KL loss
+xent_loss = K.sum(K.binary_crossentropy(x, x_decoded_mean), axis=-1)
+
+# 只需要修改K.square(z_mean)为K.square(z_mean - yh),也就是让隐变量向类内均值看齐
+kl_loss = -0.5 * K.sum(1 + z_log_var - K.square(z_mean - yh) - K.exp(z_log_var), axis=-1)
+vae_loss = K.mean(xent_loss + kl_loss)
+
+vae.add_loss(vae_loss)
+vae.compile(optimizer='rmsprop')
+vae.summary()
+
+# 开始训练
+vae.fit(
+    [x_train, y_train],
+    shuffle=True,
+    epochs=epochs,
+    batch_size=batch_size,
+    validation_data=([x_test, y_test], None)
+)
+
+# 构建encoder，然后观察各个数字在隐空间的分布
+encoder = Model(x, z_mean)
+
+x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
+plt.figure(figsize=(6, 6))
+plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test_)
+plt.colorbar()
+plt.show()
+
+# 构建生成器
+decoder_input = Input(shape=(latent_dim,))
+_h_decoded = decoder_h(decoder_input)
+_x_decoded_mean = decoder_mean(_h_decoded)
+generator = Model(decoder_input, _x_decoded_mean)
+
+# 输出每个类的均值向量
+mu = Model(y, yh)
+mu = mu.predict(np.eye(num_classes))
+
+# 观察能否通过控制隐变量的均值来输出特定类别的数字
+n = 15
+digit_size = 28
+figure = np.zeros((digit_size * n, digit_size * n))
+
+output_digit = 9  # 指定输出数字
+
+# 用正态分布的分位数来构建隐变量对
+grid_x = norm.ppf(np.linspace(0.05, 0.95, n)) + mu[output_digit][1]
+grid_y = norm.ppf(np.linspace(0.05, 0.95, n)) + mu[output_digit][0]
+
+for i, yi in enumerate(grid_x):
+    for j, xi in enumerate(grid_y):
+        z_sample = np.array([[xi, yi]])
+        x_decoded = generator.predict(z_sample)
+        digit = x_decoded[0].reshape(digit_size, digit_size)
+        figure[i*digit_size:(i+1)*digit_size, j*digit_size:(j+1)*digit_size] = digit
+
+plt.figure(figsize=(10, 10))
+plt.imshow(figure, cmap='Greys_r')
+plt.show()
